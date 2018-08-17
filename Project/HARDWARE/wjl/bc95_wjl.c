1ec3d63619b1d67a0484a08cd77137c4af9ab341
unknown <1978707987@qq.com>
2018-08-17 15:03:36 +0800

    添加了bc95_init_new函数,用于发送初始化指令 依然无法发送数据

--- Project/HARDWARE/wjl/bc95_wjl.c
+++ Project/HARDWARE/wjl/bc95_wjl.c
@@ -26,12 +26,14 @@ void BC95_Init_wjl(void)
 	while(strx==NULL)
 	{
 		Clear_Buffer();	
-		printf("AT+CMEE=1\r\n"); 
-		Delay(300);
+//		printf("AT+CMEE=1\r\n"); 
+//		Delay(300);
 		strx = "";
 		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 	
 	printf("AT+CFUN=0\r\n"); 		//发送AT指令
 	Delay(300);
@@ -41,12 +43,14 @@ void BC95_Init_wjl(void)
 	while(strx==NULL)
 	{
 		Clear_Buffer();	
-		printf("AT+CFUN=0\r\n"); 
-		Delay(300);
+//		printf("AT+CFUN=0\r\n"); 
+//		Delay(300);
 		strx = "";
 		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 	
 	printf("AT+CGSN=1\r\n"); 		//发送AT指令
 	Delay(300);
@@ -56,27 +60,31 @@ void BC95_Init_wjl(void)
 	while(strx==NULL)
 	{
 		Clear_Buffer();	
-		printf("AT+CGSN=1\r\n"); 
-		Delay(300);
+//		printf("AT+CGSN=1\r\n"); 
+//		Delay(300);
 		strx = "";
 		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 	
 	printf("AT+NCDP=180.101.147.115,5683\r\n"); 		//发送AT指令
 	Delay(300);
 	strx = "";
-	strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
+	strx=strstr((const char*)RxBuffer,(const char*)"N:");//返回OK
 	Clear_Buffer();	
 	while(strx==NULL)
 	{
 		Clear_Buffer();	
-		printf("AT+NCDP=180.101.147.115,5683\r\n"); 
-		Delay(300);
+//		printf("AT+NCDP=180.101.147.115,5683\r\n"); 
+//		Delay(300);
 		strx = "";
-		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
+		strx=strstr((const char*)RxBuffer,(const char*)"N:");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 	
 	printf("AT+CFUN=1\r\n"); 		//发送AT指令
 	Delay(300);
@@ -86,12 +94,14 @@ void BC95_Init_wjl(void)
 	while(strx==NULL || str_contain(strx, "ERROR", strlen(strx), strlen("ERROR")) == 1)
 	{
 		Clear_Buffer();	
-		printf("AT+CFUN=1\r\n"); 
-		Delay(300);
+//		printf("AT+CFUN=1\r\n"); 
+//		Delay(300);
 		strx = "";
 		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 	
 	printf("AT+CGATT=1\r\n"); 		//发送AT指令
 	Delay(300);
@@ -102,18 +112,22 @@ void BC95_Init_wjl(void)
 	//while(strcmp(strx, "OK\r\n\r\n+CEREG:2\r\n1106\r\n\r\nOK\r\n") == 0)
 	{
 		Clear_Buffer();	
-		printf("AT+CGATT=1\r\n"); 
-		Delay(300);
+//		printf("AT+CGATT=1\r\n"); 
+//		Delay(300);
 		strx = "";
 		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 	}
-	output_usart1(strx);
+	output_usart1(RxBuffer);
 	
+	Delay(500);
+	
+	Clear_Buffer();
 	printf("AT+NMGS=21,01031001010208000040004000141C0600000068BD"); 		//发送AT指令
 	//Delay(300);
 	strx = "";
+	
 	strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
-	Clear_Buffer();	
+		
 //	while(str_contain(strx, "+CEREG:2", strlen(strx), strlen("+CEREG:2")) == 1 || str_contain(strx, "ERROR", strlen(strx), strlen("ERROR")) == 1)
 //	//while(strcmp(strx, "OK\r\n\r\n+CEREG:2\r\n1106\r\n\r\nOK\r\n") == 0)
 //	{
@@ -123,7 +137,10 @@ void BC95_Init_wjl(void)
 //		strx = "";
 //		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
 //	}
-	output_usart1(strx);
+	output_usart1("strx:  ");
+	output_usart1(RxBuffer);
+	
+	Delay(500);
 }
 
 void output_usart1(char *p_str)
@@ -151,5 +168,54 @@ int str_contain(char *str_1, char *str_2, int size_1, int size_2)
 	}
 	return 0;//如果没有子字符串,返回0
 }
+
+void send_printRec(char *str_send)
+{
+	printf("%s", str_send); 		//发送AT指令
+	Delay(300);
+	strx = "";
+	strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
+	Clear_Buffer();	
+	while(strx==NULL)
+	{
+		Clear_Buffer();	
+		printf("%s", str_send); 
+		Delay(300);
+		strx = "";
+		strx=strstr((const char*)RxBuffer,(const char*)"OK");//返回OK
+	}
+	output_usart1(RxBuffer);
+}
+void send_printRec_2(char *str_send)
+{
+	printf("%s", str_send); 		//发送AT指令
+	Delay(300);
+	//strx = "";
+	strx=strstr((const char*)RxBuffer,(const char*)"OK");//检测字符串中是否有OK,如果有,返回OK之后的字符串,反之返回空
+	//Clear_Buffer();	
+	while(strx==NULL)//循环直到RxBuffer中含有OK
+	{
+		strx=strstr((const char*)RxBuffer,(const char*)"OK");//检测字符串中是否有OK,如果有,返回OK之后的字符串,反之返回空
+	}
+	output_usart1(RxBuffer);
+	Clear_Buffer();//打印之后清除缓存
+}
+
+void bc95_init_new(void)
+{
+	send_printRec_2("AT+CMEE=1\r\n");
+	send_printRec_2("AT+CFUN=0\r\n");
+	send_printRec_2("AT+CGSN=1\r\n");
+	send_printRec_2("AT+NCDP=180.101.147.115,5683\r\n");
+	send_printRec_2("AT+CFUN=1\r\n");
+	send_printRec_2("AT+NBAND=5\r\n");//之后需要确认一下是不是5
+	send_printRec_2("AT+CGATT=1\r\n");
+	send_printRec_2("AT+CGPADDR\r\n");
+	send_printRec_2("AT+NMGS=21,01031001010208000040004000141C0600000068BD");
+	send_printRec_2("AT+NMGS=21,01031001010208000040004000141C0600000068BD");
+	send_printRec_2("AT+NMGS=21,01031001010208000040004000141C0600000068BD");
+}
+
+
 
 
--- Project/HARDWARE/wjl/bc95_wjl.h
+++ Project/HARDWARE/wjl/bc95_wjl.h
@@ -9,6 +9,7 @@ void Clear_Buffer_wjl(void);
 void BC95_Init_wjl(void);
 void output_usart1(char *p_str);
 int str_contain(char *str_1, char *str_2, int size_1, int size_2);
-
+void send_printRec(char *str_send);
+void bc95_init_new(void);
 
 #endif
